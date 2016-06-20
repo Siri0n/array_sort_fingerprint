d9dd269a562d0cd0f8bdd30df6195919b62c06da
Siri0n <infernalsirion@yandex.ru>
2016-06-20 16:07:46 +0300

    Let's go

---
+++ main.js
@@ -0,0 +1,148 @@
+var minsize = 256;
+
+function hslToRgb(h, s, l){ //stolen from StackOverflow =)
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function Item(index, val, comparisons){
+	this.getVal = function(){
+		return val;
+	}
+	this.getIndex = function(){
+		return index;
+	}
+	this.compare = function(other){
+		comparisons.push([index, other.getIndex()]);
+		return val - other.getVal();
+	}
+}
+
+function shuffle(array) { //also stolen from StackOverflow
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function ensureSorted(arr, name){
+	for(var i = 0; i < arr.length - 1; i++){
+		if(arr[i].getVal() > arr[i+1].getVal()){
+			console.log("incorrect sort: " + name);
+			console.log(arr.map(function(elem){return elem.getIndex()}));
+			return;
+		}
+	}
+}
+
+function generateComparisonsArray(size, sort){
+	var comparisons = [];
+	var arr = [];
+	var i, val = 0;
+	for(i = 0; i < size; i++){
+		arr.push(new Item(i, val, comparisons));
+		val += Math.random();
+	}
+	shuffle(arr);
+	sort.sort(arr, function(a, b){ return a.compare(b)});
+	ensureSorted(arr, sort.name);
+	return comparisons;
+}
+
+function draw(arr, size, sort, parent){
+	var div = document.createElement("div");
+	parent.appendChild(div);
+	var a = document.createElement("a");
+	a.innerText = sort.name;
+	a.href=sort.href;
+	div.appendChild(a);
+	var canvas = document.createElement("canvas");
+	canvas.width = canvas.height = Math.max(size, minsize);
+	div.appendChild(canvas);
+	var ctx = canvas.getContext("2d");
+	var data = ctx.createImageData(size, size);
+	arr.forEach(function(item, index){
+		var rgb = hslToRgb(index/(arr.length*1.5), 1, 0.5);
+		var pos = size*item[0] + item[1];
+		data.data[pos*4] = rgb[0];
+		data.data[pos*4 + 1] = rgb[1];
+		data.data[pos*4 + 2] = rgb[2];
+		data.data[pos*4 + 3] = 255;
+		pos = size*item[1] + item[0];
+		data.data[pos*4] = rgb[0];
+		data.data[pos*4 + 1] = rgb[1];
+		data.data[pos*4 + 2] = rgb[2];
+		data.data[pos*4 + 3] = 255;
+	});
+	ctx.putImageData(data, 0, 0);
+	if(size < minsize){
+		var tmp = document.createElement("canvas");
+		tmp.height = tmp.width = minsize;
+		var tmpctx = tmp.getContext("2d");
+		tmpctx.drawImage(canvas, 0, 0, minsize*minsize/size, minsize*minsize/size);
+		ctx.clearRect(0, 0, minsize, minsize);
+		ctx.drawImage(tmp, 0, 0, minsize, minsize);
+	}
+}
+
+function createFingerprint(size, sort, parent){
+	var arr = generateComparisonsArray(size, sort);
+	draw(arr, size, sort, parent);
+}
+
+function nativeSort(arr, f){arr.sort(f)}
+
+
+
+function main(sorts){
+	var pow = (window.location.search.match(/size=([0-9]+)/) || [,7])[1];
+	var size = Math.pow(2, pow|0);
+	if(!(size == size)){
+		size = 128;
+	}
+	var leftColumn = document.getElementById("left");
+	var rightColumn = document.getElementById("right");
+	createFingerprint(
+		size, 
+		{
+			name: "Array#sort (current browser)", 
+			sort: nativeSort,
+			href: "http://www.w3schools.com/Jsref/jsref_sort.asp"
+		}, 
+		leftColumn);
+	sorts.forEach(function(sort){
+		createFingerprint(size, sort, rightColumn);
+	});
+}
---
+++ sort.js
@@ -0,0 +1,240 @@
+//just a helpful function
+
+function swap(arr, i, j){
+	var temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+// Selection sort
+
+function minSort(arr, comp){
+	var i, j, min, minIndex;
+	for(i = 0; i < arr.length - 1; i++){
+		min = arr[i];
+		minIndex = i;	
+		for(j = i + 1; j < arr.length; j++){
+			if(comp(min, arr[j]) > 0){
+				min = arr[j];
+				minIndex = j;
+			}
+		}
+		arr[minIndex] = arr[i];
+		arr[i] = min;
+	}
+}
+
+// Bubble sort
+
+function bubbleSort(arr, comp){
+	var i, j, temp;
+	for(i = arr.length - 1; i > 0; i--){
+		for(j = 0; j < i; j++){
+			if(comp(arr[j], arr[j + 1]) > 0){
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+//Insertion sort
+
+function insertionSort(arr, comp){
+	var i, j, k, temp;
+	for(i = 1; i < arr.length; i++){
+		j = 0;
+		while(j < i && comp(arr[j], arr[i]) <= 0){
+			j++;
+		}
+		if(j != i){
+			temp = arr[i];
+			k = i;
+			while(k > j){
+				arr[k] = arr[k-1];
+				k--;
+			}
+			arr[j] = temp;
+		}
+	}
+}
+
+//Merge sort
+
+function mergeSort(arr, comp, start, end){
+	start = start || 0;
+	end = end || arr.length;
+	if(end - start > 1){
+		var mid = Math.floor((end + start)/2);
+		mergeSort(arr, comp, start, mid);
+		mergeSort(arr, comp, mid, end);
+		merge(arr, comp, start, mid, end);
+	}
+}
+
+function merge(arr, comp, start, mid, end){
+	var i = start, j = mid, arr_temp = [];
+	while(i < mid && j < end){
+		if(comp(arr[i], arr[j]) < 0){
+			arr_temp.push(arr[i++]);
+		}else{
+			arr_temp.push(arr[j++]);
+		}
+	}
+	var k, maxk;
+	if(i == mid){
+		k = j;
+		maxk = end;
+	}else{
+		k = i;
+		maxk = mid;
+	}
+	arr_temp = arr_temp.concat(arr.slice(k, maxk));
+	for(i = start, j = 0; i < end; i++, j++){
+		arr[i] = arr_temp[j];
+	}
+
+}
+
+//Quicksort (without optimizations)
+
+function quickSort(arr, comp, start, end){
+	start = start || 0;
+	end = end || arr.length;
+	if(start >= end - 1){
+		return;
+	}
+	var pivot = arr[end - 1];
+	var i = start, j = end - 1, temp;
+	while(true){
+		while(i < end && comp(arr[i], pivot) < 0){
+			i++;
+		}
+		while(j >= start && comp(arr[j], pivot) > 0){
+			j--;
+		}
+		if(i > j){
+			break;
+		}
+		swap(arr, i, j);
+		i++;
+		j--;
+	}
+
+	quickSortMedian(arr, comp, start, i);
+	quickSortMedian(arr, comp, i, end);
+
+}
+
+//Quicksort (median pivot)
+
+function quickSortMedian(arr, comp, start, end){
+	start = start || 0;
+	end = end || arr.length;
+	if(start >= end - 1){
+		return;
+	}
+	var i = start, j = end - 1, temp;
+	var pivots = [
+		arr[start],
+		arr[end - 1],
+		arr[Math.floor((start + end - 1)/2)]
+	];
+	if(comp(pivots[0], pivots[1]) > 0){
+		swap(pivots, 0, 1);
+	}
+	if(comp(pivots[1], pivots[2]) > 0){
+		swap(pivots, 1, 2);
+	}
+	if(comp(pivots[0], pivots[1]) > 0){
+		swap(pivots, 0, 1);
+	}
+	var pivot = pivots[1];
+	while(true){
+		while(i < end && comp(arr[i], pivot) < 0){
+			i++;
+		}
+		while(j >= start && comp(arr[j], pivot) > 0){
+			j--;
+		}
+		if(i > j){
+			break;
+		}
+		swap(arr, i, j);
+		i++;
+		j--;
+	}
+
+	quickSortMedian(arr, comp, start, i);
+	quickSortMedian(arr, comp, i, end);
+
+}
+
+//Heapsort
+
+function heapSort(arr, comp){
+	var i = Math.floor((arr.length)/2);
+	while(i >= 0){
+		shiftDown(arr, comp, i, arr.length - 1);
+		i--;
+	}
+	for(i = arr.length - 1; i > 0; i--){
+		swap(arr, i, 0);
+		shiftDown(arr, comp, 0, i - 1);
+	}
+}
+
+function shiftDown(arr, comp, n, maxheap){
+	if(2*n + 1 > maxheap){
+		return;
+	}
+	var maxchild;
+	if(2*n + 2 > maxheap){
+		maxchild = 2*n + 1;
+	}else{
+		maxchild = (comp(arr[2*n + 1], arr[2*n + 2]) > 0) ? 2*n + 1 : 2*n + 2;
+	}
+	if(comp(arr[n], arr[maxchild]) < 0){
+		swap(arr, n, maxchild);
+		shiftDown(arr, comp, maxchild, maxheap);
+	}
+}
+
+var sorts = [
+	{
+		name: "Selection sort",
+		sort: minSort,
+		href: "https://en.wikipedia.org/wiki/Selection_sort"
+	},
+	{
+		name: "Bubble sort",
+		sort: bubbleSort,
+		href: "https://en.wikipedia.org/wiki/Bubble_sort"
+	},
+	{
+		name: "Insertion sort",
+		sort: insertionSort,
+		href: "https://en.wikipedia.org/wiki/Insertion_sort"
+	},
+	{
+		name: "Merge sort",
+		sort: mergeSort,
+		href: "https://en.wikipedia.org/wiki/Merge_sort"
+	},
+	{
+		name: "Quicksort (unoptimized)",
+		sort: quickSort,
+		href: "https://en.wikipedia.org/wiki/Quicksort"
+	},
+	{
+		name: "Quicksort (median pivot)",
+		sort: quickSortMedian,
+		href: "https://en.wikipedia.org/wiki/Quicksort#Choice_of_pivot"
+	},
+	{
+		name: "Heapsort",
+		sort: heapSort,
+		href: "https://en.wikipedia.org/wiki/Heapsort"
+	}
+]
